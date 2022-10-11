c2960a5c1221e8ead5464ad2a8e7f935d1bf13c6
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-11 08:53:28 +0800

    prettier implementation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const process = require('node:process');
 
-const [,,...args] = process.argv;
-const mergedStr = args.join(" ");
+const [, , ...args] = process.argv;
+const mergedStr = args.join(' ');
 let vcount = 0;
 
-for(const letters of mergedStr) {
-    if('aeiou'.includes(letters.toLowerCase())) vcount++;
+for (const letters of mergedStr) {
+  if ('aeiou'.includes(letters.toLowerCase())) vcount++;
 }
 
 console.log('Number of arguments: ' + args.length);
